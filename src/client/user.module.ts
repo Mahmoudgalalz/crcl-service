d7decf105d41e054f7f164bca4176e4a7e45b304
Mahmoud Galal <ssss.aaa2002@gmail.com>
2024-11-03 04:29:49 +0200

    adding bcrypt service

--- src/client/user.module.ts
+++ src/client/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { BoothController } from './booth.controller';
 import { ReaderController } from './reader.controller';
+import { BcryptService } from 'src/shared/auth/shared/bcrypt.service';
 
 @Module({
   controllers: [UserController, BoothController, ReaderController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, BcryptService],
 })
 export class UserModule {}
